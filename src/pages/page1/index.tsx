a5c0103b6c329e86d2311c97b2076a9ca1028196
reggiegunawan88 <reggiegunawan88@gmail.com>
2022-10-10 17:34:12 +0700

    docs: create dummy jsx component

--- src/pages/page1/index.tsx
+++ src/pages/page1/index.tsx
@@ -1,21 +1,17 @@
-import { ReactNode } from 'react';
 
 import MainLayout from '@/layout/MainLayout';
-
 import FirstParagraph from './components/FirstParagraph';
 import SecondParagraph from './components/SecondParagraph/index.';
 
 const About = () => {
   return (
+    <MainLayout>
     <div>
       <FirstParagraph />
       <SecondParagraph />
     </div>
+    </MainLayout>
   );
-};
-
-About.getLayout = (page: ReactNode) => {
-  return <MainLayout>{page}</MainLayout>;
 };
 
 export default About;
