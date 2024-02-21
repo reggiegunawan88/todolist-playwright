bd551c22711fb4a7a4eccbed777151fec04c531b
reggiegunawan <reggie.maurice@accelbyte.net>
2024-02-21 14:36:00 +0700

    feat(e2e): update test case

--- src/e2e/NegativeCase.spec.ts
+++ src/e2e/NegativeCase.spec.ts
@@ -6,44 +6,60 @@ test.beforeEach(async ({ page }) => {
 
 test.describe('Test negative cases', () => {
   test('Test submit if both input are empty', async ({ page }) => {
+    // 1. Define input fields and submit button
     const titleInput = page.getByLabel('task-title-input')
     const hoursInput = page.getByLabel('task-hours-input')
     const submitInputBtn = page.getByLabel('add-input-button')
 
+    // 2. Both input fields should have empty value
     await expect(titleInput).toHaveValue('')
     await expect(hoursInput).toHaveValue('')
 
+    // 3. Click submit button
     await submitInputBtn.click()
 
+    // 4. Define error text for both inputs
     const errTitleInput = page.getByLabel('title-input-error-text')
     const errHoursInput = page.getByLabel('hours-input-error-text')
+
+    // 5. Define error snackbar
     const errSnackbar = page.getByLabel('snackbar-ui')
 
+    // 6. Error text and snackbar should appear
     await expect(errTitleInput).toBeVisible()
     await expect(errHoursInput).toBeVisible()
     await expect(errSnackbar).toBeVisible()
+    await expect(errSnackbar).toHaveText('Please fill the input value')
 
+    // 7. Snackbar should disappear after 3s
     await expect(errSnackbar).not.toBeVisible({ timeout: 3000 })
   })
 
   test('Test filled title input and empty hours input or vice-versa', async ({ page }) => {
+    // 1. Define input fields and submit button
     const titleInput = page.getByLabel('task-title-input')
     const hoursInput = page.getByLabel('task-hours-input')
     const submitInputBtn = page.getByLabel('add-input-button')
 
+    // 2. Fill title input only
     titleInput.fill('Task Title 1')
     expect(hoursInput).toHaveValue('')
 
+    // 3. Click submit button
     await submitInputBtn.click()
 
+    // 4. Define error text for both input and error snackbar
     const errTitleInput = page.getByLabel('title-input-error-text')
     const errHoursInput = page.getByLabel('hours-input-error-text')
     const errSnackbar = page.getByLabel('snackbar-ui')
 
+    // 5. Since we already fill title input, error text should appear on hours input
     await expect(errTitleInput).not.toBeVisible()
     await expect(errHoursInput).toBeVisible()
     await expect(errSnackbar).toBeVisible()
+    await expect(errSnackbar).toHaveText('Please fill the input value')
 
+    // 6. Snackbar should disappear after 3s
     await expect(errSnackbar).not.toBeVisible({ timeout: 3000 })
   })
 })
--- src/e2e/PositiveCase.spec.ts
+++ src/e2e/PositiveCase.spec.ts
@@ -7,62 +7,86 @@ test.beforeEach(async ({page}) => {
 
 test.describe('Test Positive cases', () => {
   test('Test success submit', async ({ page }) => {
+    // 1. Define both input and submit button
     const titleInput = page.getByLabel('task-title-input')
     const hoursInput = page.getByLabel('task-hours-input')
     const submitInputBtn = page.getByLabel('add-input-button')
 
+    // 2. Define tbody content
     let tableTodoValue = await page.locator("//table/tbody/tr[1]/td[1]").innerText()
+    
+    // 3. For initial state, table content should contains "You don't have any to-do item."
     await expect(tableTodoValue).toBe("You don't have any to-do item.")
 
+    // 4. Fill both inputs
     await titleInput.fill('Task Title 1')
     await hoursInput.fill('12')
     await expect(hoursInput).toHaveValue(/[0-9]/)
 
+    // 5. Click submit button
     await submitInputBtn.click()
 
+    // 6. Both error text should not appear
     const errTitleInput = page.getByLabel('title-input-error-text')
     const errHoursInput = page.getByLabel('hours-input-error-text')
     const successSnackbar = page.getByLabel('snackbar-ui')
 
     await expect(errTitleInput).not.toBeVisible()
     await expect(errHoursInput).not.toBeVisible()
+    
+    // 7. Show success snackbar
     await expect(successSnackbar).toBeVisible()
 
+    // 8. Table tbody should contains the newest data entry
     tableTodoValue = await page.locator("//table/tbody/tr[1]/td[1]").innerText()
     await expect(tableTodoValue).toBe("Task Title 1")
   })
 
   test('Test delete todo', async ({ page }) => {
+    // 1. Define both input and submit button
     const titleInput = page.getByLabel('task-title-input')
     const hoursInput = page.getByLabel('task-hours-input')
     const submitInputBtn = page.getByLabel('add-input-button')
 
+    // 2. Fill both input
     await titleInput.fill('Task Title 1')
     await hoursInput.fill('12')
 
+    // 3. Click submit button
     await submitInputBtn.click()
 
+    // 4. Define delete button and click action
     const deleteButton = page.getByLabel('delete-todo-button')
 
     await deleteButton.click()
 
+    // 5. Delete dialog modal should appear
     const deleteDialog = page.getByLabel('delete-modal-dialog')
     await expect(deleteDialog).toBeVisible()
     
+    // 6. Define delete confirmation input, task title, and submit button
     const deleteConfirmationInput = await page.getByLabel('confirmation-input')
     const deleteDialogTaskTitle = await page.getByLabel('delete-dialog-task-title').innerText()
     const deleteSubmitButton = await page.getByLabel('delete-dialog-submit-button')
     
+    // 7. On initial condition, delete submit button should be disabled
     await expect(deleteSubmitButton).toBeDisabled()
     
+    // 8. Fill confirmation input
     deleteConfirmationInput.fill('Task Title 1')
-
     await expect(deleteConfirmationInput).toHaveValue(deleteDialogTaskTitle)
 
+    // 9. After confirmation input is filled, delete button should not be disabled anymore
     await expect(deleteSubmitButton).not.toBeDisabled()
 
+    // 10. Click delete submit button
     deleteSubmitButton.click()
 
+    // 11. After deletion success, delete dialog modal should disappear
     await expect(deleteDialog).not.toBeVisible()
+
+    // 12. Table tbody back to empty state
+    const tableContent = await page.locator("//table/tbody/tr[1]/td[1]").innerText()
+    await expect(tableContent).toBe("You don't have any to-do item.")
   })
 })
